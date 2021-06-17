49d715a631bf68085515b1beda780dd7b9694ae7
illuminatus52 <78482467+illuminatus52@users.noreply.github.com>
2021-06-17 11:55:25 +0200

    init rsd-movieshop

---
+++ src/main/java/com/rsd_movieshop/RsdMovieshopApplication.java
@@ -0,0 +1,13 @@
+package com.rsd_movieshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsdMovieshopApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RsdMovieshopApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/rsd_movieshop/RsdMovieshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsd_movieshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsdMovieshopApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
