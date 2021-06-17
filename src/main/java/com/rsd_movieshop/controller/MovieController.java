d3dc0dc85213d9cce1e3975039a13d07857a8601
illuminatus52 <78482467+illuminatus52@users.noreply.github.com>
2021-06-17 15:54:11 +0200

    firstMapping

--- src/main/java/com/rsd_movieshop/controller/MovieController.java
+++ src/main/java/com/rsd_movieshop/controller/MovieController.java
@@ -1,18 +1,31 @@
 package com.rsd_movieshop.controller;
 
-import org.springframework.stereotype.Controller;
+import com.rsd_movieshop.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class MovieController {
-   @GetMapping ("/")
+    
+    @GetMapping ("/")
     public String getIndex() {
-       return "";
-   }
+        return "";
+    }
     @GetMapping   ("/register")
     public String getRegister() {
         return "";
+    }
+    
+    @GetMapping("/user/{userID}")
+    public User getUser(@PathVariable int userID) {
+        // TODO DB-Abfrage auf userID
+        
+    }
+    
+    @GetMapping ("/cart")
+    public Cart getCart(userID) {
+        // get user cart
     }
 }
